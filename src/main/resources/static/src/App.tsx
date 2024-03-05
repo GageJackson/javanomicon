f5c72b0c0a01b6a00cf6ba495bea5b24ddc5cc26
GageJackson <wm.gagejackson@gmail.com>
2024-03-05 12:26:20 -0600

    i will get this to work

---
+++ src/main/resources/static/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import {HomePage} from "./layouts/HomePage/HomePage";
+
+export const App = () => {
+    return (
+        <>
+            <HomePage/>
+        </>
+    );
+}
