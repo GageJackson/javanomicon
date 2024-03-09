b99fe675c4a724d0cfd6d70f87d3cfbc8784b9ca
GageJackson <wm.gagejackson@gmail.com>
2024-03-08 23:58:51 -0600

    setting up tasting services and controller

---
+++ src/main/java/com/javanomicon/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.javanomicon.controller;
+
+import com.javanomicon.service.TastingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+
+
+}
