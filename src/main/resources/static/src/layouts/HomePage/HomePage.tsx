01368e2a706425c9293d7bebbc0b1f470a04d3e5
GageJackson <wm.gagejackson@gmail.com>
2024-03-09 16:25:09 -0600

    working on connecting premade tastings to front-end

--- src/main/resources/static/src/layouts/HomePage/HomePage.tsx
+++ src/main/resources/static/src/layouts/HomePage/HomePage.tsx
@@ -1,7 +1,72 @@
 import React from "react";
 import {useEffect, useState} from "react";
+import CoffeeModel from "../../../models/CoffeeModel";
+import {SpinnerLoading} from "../../Utils/SpinnerLoading";
 
 export const HomePage = () => {
+
+    const [tastings, setTastings] = useState<TastingModel[]>([])
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    useEffect(() => {
+        const fetchTastings = async () => {
+            const baseUrl: string = "http://localhost:8080/api/tastings";
+            const url: string = `${baseUrl}?page=0&size=9`;
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error('Something went wrong fetching tastings from API');
+            }
+
+            const responseJson = await response.json();
+            const responseData = responseJson._embedded.tastings;
+            const loadedTastings: TastingModel[] = [];
+
+            for (const key in responseData){
+                loadedTastings.push(new TastingModel(
+                    responseData[key].id,
+                    responseData[key].tastingDate,
+                    responseData[key].overallScore,
+                    responseData[key].description,
+                    responseData[key].tastingFlavors,
+                    responseData[key].scoreAcidity,
+                    responseData[key].scoreAroma,
+                    responseData[key].scoreBalance,
+                    responseData[key].scoreBody,
+                    responseData[key].scoreFinish,
+                    responseData[key].scoreFlavor,
+                    responseData[key].scoreRoast,
+                    responseData[key].scoreSweetness,
+                    responseData[key].coffeeDetails,
+                    responseData[key].brewDetails
+                ));
+            }
+
+            setTastings(loadedTastings);
+            setIsLoading(false);
+        };
+
+        fetchTastings().catch((error:any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);
+
+    if (isLoading){
+        return (
+            <SpinnerLoading/>
+        )
+    }
+
+    if (httpError){
+        return (
+            <div className={'container m-5'}>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
     return (
         <div>
             <h1>hello</h1>
